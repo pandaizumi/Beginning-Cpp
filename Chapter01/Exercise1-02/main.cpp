ea8d15a3d178a119e61c855e08f15ba0b6f70827
XL'anna Brooks <izumichan31@gmail.com>
2021-02-12 15:09:58 -0500

    Exercise 1-2

---
+++ Chapter01/Exercise1-02/main.cpp
@@ -0,0 +1,9 @@
+// Exercise 1-2: Create and execute a program that outputs your name one line
+// and your age on the next
+#include <iostream>
+
+int main() {
+    std::cout << "Name: XL'anna Brooks\n";
+    std::cout << "Age: 31" << std::endl;
+    return 0;
+}
