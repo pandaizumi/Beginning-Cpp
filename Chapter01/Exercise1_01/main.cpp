8026d5d9a3d6a66bb30e4c0783145777f2fdca02
XL'anna Brooks <izumichan31@gmail.com>
2021-02-12 15:02:04 -0500

    Exercise 1-1

---
+++ Chapter01/Exercise1_01/main.cpp
@@ -0,0 +1,8 @@
+// Exercise 1-1: Create, compile. link, and execute a program
+// that displays "Hello World"
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
